970e5b5abd96ab67bf09192d9069e1e48d6904e8
Federico Gobea <fgobea@chroma.agency>
2025-10-31 02:29:24 -0500

    Eliminar el módulo 'sale_advance_payment_inv' y ajustar la importación en '__init__.py'

--- ceg/wizard/__init__.py
+++ ceg/wizard/__init__.py
@@ -1,2 +1,1 @@
 from . import purchase_advance_payment_inv
-from . import sale_advance_payment_inv
--- ceg/wizard/sale_advance_payment_inv.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-class SaleAdvancePaymentInv(models.TransientModel):
-    _inherit = 'sale.advance.payment.inv'
-
-    def _prepare_base_downpayment_line_values(self, order):
-        vals = super()._prepare_base_downpayment_line_values(order)
-        vals['product_id'] = self.env.ref('ceg.anticipo_product').id
-        return vals
