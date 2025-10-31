717245a7e06d9d9c104a7b1b3c3ac98c27fa406c
Federico Gobea <fgobea@chroma.agency>
2025-10-31 02:50:56 -0500

    Agregar método para obtener el precio unitario de la línea de pago anticipado en el modelo 'sale.order.line'

--- ceg/models/sale_order.py
+++ ceg/models/sale_order.py
@@ -79,3 +79,8 @@ class SaleOrder(models.Model):
                 'sticky': False,
             }
         }
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    def _get_downpayment_line_price_unit(self, invoices):
+        return self.price_unit
