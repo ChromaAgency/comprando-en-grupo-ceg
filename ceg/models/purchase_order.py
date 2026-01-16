1bf4b444ca7d0c0dabdfb88815ddf87bc042db2c
Federico Gobea <fgobea@chroma.agency>
2026-01-16 05:31:23 -0500

    Agregar registro de información al verificar órdenes de venta en '_get_magento_instance' para facilitar la depuración.

--- ceg/models/purchase_order.py
+++ ceg/models/purchase_order.py
@@ -41,6 +41,7 @@ class PurchaseOrder(models.Model):
         
         
         for sale_order in self.order_line.move_dest_ids.picking_id.sale_id:
+            _logger.info("Checking sale order %s for magento_instance_id", sale_order.name)
             # Verificar si hay una relación indirecta a través de movimientos de stock
             if (hasattr(sale_order, 'magento_instance_id') and 
                 sale_order.magento_instance_id):
