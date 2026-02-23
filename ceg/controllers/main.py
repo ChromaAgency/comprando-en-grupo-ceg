69bd3a18efc5e1f9a2b063f3a37bc689373cd71d
Federico Gobea <fgobea@chroma.agency>
2026-02-23 15:32:00 -0500

    Actualizar campos de dirección en la función _add_row_data_to_csv para incluir el número de calle

--- ceg/controllers/main.py
+++ ceg/controllers/main.py
@@ -42,7 +42,7 @@ class ReportSaleOperationsTradeUnity(Controller):
         OPERATION_NUMBER = ''
         #Referencia, Transportista, Direccion Transportista quedo vacio
         data = [sale_order.name, customer_commercial.id, customer.display_name, customer_phone,customer_mobile, product.ceg_code or '', product_component.ceg_code or '',
-                   product.name, UNIDAD, move_qty*box_qty, move_qty,move_qty*product.volume, REFERENCIA,delivery_mode,shipping_address.contact_address_complete, shipping_address.street, 
+                   product.name, UNIDAD, move_qty*box_qty, move_qty,move_qty*product.volume, REFERENCIA,delivery_mode,shipping_address.contact_address_complete, shipping_address.street_name, shipping_address.street_number,
                    shipping_address.street2, shipping_address.city, shipping_address.zip, shipping_address.state_id.name, shipping_address.country_id.name,
                      OPERATION_NUMBER,  picking.name, product.default_code, product_component_code, product_barcode]
        
@@ -56,7 +56,7 @@ class ReportSaleOperationsTradeUnity(Controller):
 
     def _add_header_to_csv(self, file_content):
         headers = ['Pedido de ventas', 'Cuenta de cliente', 'Nombre', 'Teléfono del cliente','Teléfono móvil del cliente', 'Código de artículo',	'Código de artículo componente',
-               'Nombre del producto','Unidad','Cantidad','Bultos','Volumen','Referencia','Modo de entrega','Dirección', 'Nombre de calle', 'Calle 2', 'Ciudad', 'Código Postal', 'Estado', 'País',
+               'Nombre del producto','Unidad','Cantidad','Bultos','Volumen','Referencia','Modo de entrega','Dirección', 'Nombre de calle',  'Número de calle', 'Calle 2', 'Ciudad', 'Código Postal', 'Estado', 'País',
                'Número de operación', 'Número de operación Odoo', 'SKU', 'SKU Componente', 'Código de barras'] 
         file_content.write(SEPARATOR.join(headers))
         file_content.write('\n')
